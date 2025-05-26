20d14fd0428217ffa30c234258d18209d17551f8
jsabadell <508963@student.fontys.nl>
2025-05-26 19:36:41 +0200

    some routing, pages, and assets

--- TestApp/app/(tabs)/index.tsx
+++ TestApp/app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
-import onboarding from "./onboarding";
 
 
 export default function Index() {
@@ -8,7 +7,7 @@ export default function Index() {
     <View className="flex-1 justify-center items-center">
 
       <Text className="text-5xl text-dark-200 font-bold">Vambora pae.</Text>
-      <Link href="/onboarding">Onboarding</Link>
+
     </View>
   );
 }
