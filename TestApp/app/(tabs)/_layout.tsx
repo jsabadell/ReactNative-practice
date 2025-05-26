320eb4354ba37f71186cc702a50a6a3ad286057e
jsabadell <508963@student.fontys.nl>
2025-05-26 20:30:32 +0200

    using assets for home Ui and button styling

--- TestApp/app/(tabs)/_layout.tsx
+++ TestApp/app/(tabs)/_layout.tsx
@@ -1,41 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { HeaderShownContext, HeaderTitle } from '@react-navigation/elements'
-import { Tabs } from 'expo-router'
+    import { Image, View, Text, ImageBackground } from 'react-native'
+    import React from 'react'
+    import { HeaderShownContext, HeaderTitle } from '@react-navigation/elements'
+    import { Tabs } from 'expo-router'
+    import { images } from '@/constants/images'
+    import { icons } from '@/constants/icons'
+    import play from '../../assets/icons/play.png';
 
-const _layout = () => {
-  return (
-    <Tabs>
-        <Tabs.Screen
-        name= "index"
-        options={{
-            title: 'Home',
-            headerShown: false
-        }}
-        />
-        <Tabs.Screen
-        name="search"
-        options={{
-            title: 'Search',
-            headerShown: false
-        }}
-        />
-        <Tabs.Screen
-        name="saved"
-        options={{
-            title: 'Saved',
-            headerShown: false
-        }}
-        />
-        <Tabs.Screen
-        name="profile"
-        options={{
-            title: 'Profile',
-            headerShown: false
-        }}
-        />
-    </Tabs>
-  )
-}
 
-export default _layout
+
+    const _layout = () => {
+    return (
+        <Tabs>
+            <Tabs.Screen
+            name= "index"
+            options={{
+                title: 'Home',
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <>
+                        <ImageBackground
+                            source={images.highlight}
+                            className="flex flex-row w-full flex-1 min-1-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+                            >
+                            <Image source={icons.home}
+                            tintColor="#151312" className="size-5"/>
+
+                            <Text>Home</Text>
+                        </ImageBackground>
+                    </>
+                )
+            }}
+            />
+
+            <Tabs.Screen
+            name="search"
+            options={{
+                title: 'Search',
+                headerShown: false
+            }}
+            />
+
+            <Tabs.Screen
+            name="saved"
+            options={{
+                title: 'Saved',
+                headerShown: false
+            }}
+
+            />
+            <Tabs.Screen
+            name="profile"
+            options={{
+                title: 'Profile',
+                headerShown: false
+            }}
+            />
+        </Tabs>
+    )
+    }
+
+    export default _layout
